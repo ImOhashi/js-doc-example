843915cf071921699aae4ce6cd7ba58b8dad3008
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-02-23 01:37:02 -0300

    add search methods in base repository class

---
+++ src/repositories/BaseRepository.js
@@ -0,0 +1,45 @@
+/**
+ * Base mongo CRUD class
+ *
+ * @class
+ */
+export default class BaseRepository {
+  /**
+   * Recive a model
+   *
+   * @constructor
+   * @param {Model} model
+   */
+  constructor(model) {
+    this.model = model;
+  }
+
+  /**
+   * Get document by id
+   *
+   * @public
+   * @async
+   * @memberof BaseRepository
+   * @method getById
+   * @param {string} id
+   * @returns {object}
+   */
+  async getById(id) {
+    return this.model.findOne({
+      _id: id,
+    });
+  }
+
+  /**
+   * Get all document
+   *
+   * @public
+   * @async
+   * @memberof BaseRepository
+   * @method getAll
+   * @returns {[object]}
+   */
+  async getAll() {
+    return this.model.find();
+  }
+}
